4bcdf4c9eae3b309312b85af2b7482dd150f3bd9
Fredrik Nilsson <fredrik.pj.nilsson@gmail.com>
2020-12-23 23:16:46 +0100

    d23p2

--- day23/day23.rkt
+++ day23/day23.rkt
@@ -1,42 +1,102 @@
 #lang racket
 
-(define sample-input (list 3 8 9 1 2 5 4 6 7))
 (define puzzle-input (list 1 3 5 4 6 8 7 2 9))
-(define n-cups (length puzzle-input))
+(define puzzle-input2 (append puzzle-input (range 10 1000001)))
 
-(define (move cups current-cup)
-  (let* ([current-cup-idx (index-of cups current-cup)]
-         [picked-up-cups (for/list ([idx (in-naturals (add1 current-cup-idx))]
-                                    [n (in-range 3)])
-                           (list-ref cups (modulo idx n-cups)))]
-         [remaining-cups (filter (compose not (curryr member picked-up-cups)) cups)]
-         [destination-cup (findf (curryr member remaining-cups)
-                                 (append (range (sub1 current-cup) 0 -1)
-                                         (list (apply max remaining-cups))))]
-         [destination-cup-idx (index-of remaining-cups destination-cup)]
-         [result-order (append (take remaining-cups (add1 destination-cup-idx))
-                               picked-up-cups
-                               (drop remaining-cups (add1 destination-cup-idx)))]
-         [next-current (list-ref result-order
-                                 (modulo (add1 (index-of result-order current-cup)) n-cups))])
-    (printf "cups: ~a~n" cups)
-    (printf "current cup: ~a~n" current-cup)
-    (printf "pickup: ~a~n" picked-up-cups)
-    (printf "dest: ~a~n" destination-cup)
-    (printf "dest-idx: ~a~n" destination-cup-idx)
-    (printf "next current: ~a~n" next-current)
-    (printf "next current idx: ~a~n~n" (modulo (add1 (index-of result-order current-cup)) n-cups))
-    (values result-order next-current)))
+(struct node (left right) #:transparent)
 
-(define (play cups current-cup n)
-  (let-values ([(result-cups next-current-cup) (move cups current-cup)])
+;; Doubly linked list in hashmap, for (nearly) constant T access/insertion
+(define (make-node-hash input)
+  (letrec ([helper
+            (λ (xs prev acc)
+              (cond [(empty? (cdr xs)) ;; Last element - return
+                     (hash-set acc (car xs) (node prev (first input)))]
+                    [(not prev) ;; First element
+                     (helper (cdr xs)
+                             (car xs)
+                             (hash-set acc (car xs) (node (last input) (cadr xs))))]
+                    [else
+                     (helper (cdr xs)
+                             (car xs)
+                             (hash-set acc (car xs) (node prev (cadr xs))))]))])
+    (helper input #f (hash))))
+
+(define (navigate dir-proc node-hash node n)
+  (if (equal? 1 n)
+      (dir-proc (hash-ref node-hash node))
+      (navigate dir-proc node-hash (dir-proc (hash-ref node-hash node)) (sub1 n))))
+
+(define (node-hash->list node-hash first-element)
+  (letrec ([helper
+            (λ (xs)
+              (cond [(equal? first-element (car xs)) xs]
+                    [else (helper (cons (navigate node-left node-hash (car xs) 1) xs))]))])
+    (helper (list (navigate node-left node-hash first-element 1)))))
+
+(define (move node-hash current-cup n-cups)
+  (let* ([picked-up-cups (list (navigate node-right node-hash current-cup 1)
+                               (navigate node-right node-hash current-cup 2)
+                               (navigate node-right node-hash current-cup 3))]
+         [post-pickup
+          ((compose
+            (curryr hash-remove (first picked-up-cups))
+            (curryr hash-remove (second picked-up-cups))
+            (curryr hash-remove (third picked-up-cups))
+            ;; Repair current cup
+            (curryr hash-set current-cup
+                    (node (navigate node-left node-hash current-cup 1)
+                          (navigate node-right node-hash current-cup 4)))
+            ;; Repair right of picked up cups
+            (curryr hash-set (navigate node-right node-hash current-cup 4)
+                    (node current-cup
+                          (navigate node-right node-hash current-cup 5))))
+           node-hash)]
+         [destination (for/first ([d (in-sequences (in-range (sub1 current-cup) 0 -1)
+                                                   (in-range n-cups current-cup -1))]
+                                  #:when (not (member d picked-up-cups)))
+                        d)]
+         [post-insertion
+          ((compose
+            (curryr hash-set destination
+                    (node (navigate node-left post-pickup destination 1)
+                          (first picked-up-cups)))
+            (curryr hash-set (first picked-up-cups)
+                    (node destination (second picked-up-cups)))
+            (curryr hash-set (second picked-up-cups)
+                    (node (first picked-up-cups) (third picked-up-cups)))
+            (curryr hash-set (third picked-up-cups)
+                    (node (second picked-up-cups)
+                          (navigate node-right post-pickup destination 1)))
+            (curryr hash-set (navigate node-right post-pickup destination 1)
+                    (node (third picked-up-cups)
+                          (navigate node-right post-pickup destination 2))))
+           post-pickup)]
+         [next-current (navigate node-right post-insertion current-cup 1)])
+    (values post-insertion next-current)))
+
+(define (play cups current-cup n n-cups)
+  (let-values ([(result-cups next-current-cup) (move cups current-cup n-cups)])
     (if (n . > . 1)
-        (play result-cups next-current-cup (sub1 n))
+        (play result-cups next-current-cup (sub1 n) n-cups)
         result-cups)))
 
-(define (crabstring order)
-  (string-join (map number->string (for/list ([idx (in-naturals (add1 (index-of order 1)))]
-                                              [n (in-range (sub1 n-cups))])
-                                     (list-ref order (modulo idx n-cups)))) ""))
-        
-(displayln (crabstring (play puzzle-input (car puzzle-input) 100)))
+(define (crabstring node-hash)
+  (string-join (map number->string (drop (node-hash->list node-hash 1) 1)) ""))
+
+(define (secret-product node-hash)
+  (* (navigate node-right node-hash 1 1)
+     (navigate node-right node-hash 1 2)))
+
+;; Part 1
+(displayln (crabstring (play (make-node-hash puzzle-input)
+                             (car puzzle-input)
+                             100
+                             (length puzzle-input))))
+
+;; Part 2 - Takes about 5 min to run on i9-9900K. Could optimize
+;; a bit but won't bother
+(displayln (secret-product (play (make-node-hash puzzle-input2)
+                                 (car puzzle-input2)
+                                 10000000
+                                 (length puzzle-input2))))
+
